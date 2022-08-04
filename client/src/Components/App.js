8f159a4882b0550a7a8a5125618c706dd1080360
Maxwell Bourgeois <maxwellbourgeois@gmail.com>
2022-08-04 18:46:11 -0500

    attempted to fix issue where server would not respond in time to auto-login a user.

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -15,37 +15,43 @@ export const stateContext = createContext();
 
 function App() {
   const [user, setUser] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+
   useEffect(() => {
-    fetch("/me").then((r) => {
-      if (r.ok) {
-        r.json().then((d) => setUser(d));
-      }
-    });
+    fetch("/me")
+      .then((r) => {
+        if (r.ok) {
+          r.json().then((d) => setUser(d));
+        }
+      })
+      .then(() => setLoaded(true));
   }, []);
 
   // console.log(user);
   return (
-    <stateContext.Provider value={{ user, setUser }}>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/home" element={user && <HomePage />} />
-          <Route path="/friends" element={user && <FriendsPage />} />
-          <Route path="/planner" element={user && <PlannerPage />} />
-          <Route
-            path="/planner/:username"
-            element={user && <FriendPlannerPage />}
-          />
-          <Route
-            path="/grouped-planner"
-            element={user && <GroupedPlannerPage />}
-          />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-        </Routes>
-        {/* {user && <Chat />} */}
-      </div>
-    </stateContext.Provider>
+    loaded && (
+      <stateContext.Provider value={{ user, setUser }}>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/home" element={user && <HomePage />} />
+            <Route path="/friends" element={user && <FriendsPage />} />
+            <Route path="/planner" element={user && <PlannerPage />} />
+            <Route
+              path="/planner/:username"
+              element={user && <FriendPlannerPage />}
+            />
+            <Route
+              path="/grouped-planner"
+              element={user && <GroupedPlannerPage />}
+            />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+          </Routes>
+          {/* {user && <Chat />} */}
+        </div>
+      </stateContext.Provider>
+    )
   );
 }
 
